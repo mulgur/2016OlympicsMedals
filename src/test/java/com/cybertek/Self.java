370acd4f01fe84493830f8e76c83dc755c26092c
mulgur <39856296+mulgur@users.noreply.github.com>
2018-10-11 12:09:00 -0400

    Update Self.java
    gthb

--- src/test/java/com/cybertek/Self.java
+++ src/test/java/com/cybertek/Self.java
@@ -8,9 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Self {
-	//olur oyle
-	
-	//hello world
+	//github
 	
 	public Self(WebDriver driver) {
 		PageFactory.initElements(driver, this);
